49f23e3ee9a9b41d134b5fc4762d749b7d72e3ce
Verzul-1 <vipzenit666666@gmail.com>
2021-09-14 16:25:18 +0300

    create models 3

--- cars/models.py
+++ cars/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class cars_label(models.Model):
+    name = models.CharField(verbose_name="марка авто", max_length=64)
+    country = models.CharField(verbose_name="страна производителя", max_length=64)
+
+
+
+
