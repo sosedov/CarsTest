0e62ab7c4d48b18371af3474209dfe32535ec6c4
Verzul-1 <vipzenit666666@gmail.com>
2021-09-14 13:15:37 +0300

    join postgress

--- Test_Project/settings.py
+++ Test_Project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Test_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '3714',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
